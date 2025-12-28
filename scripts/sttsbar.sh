49d51d6e9b965227fdd3e39b6ef8c8b644bb821a
William Pettersson <contact@williampettersson.eu>
2025-12-28 16:36:59 +0100

    Update sttsbar.sh and .bash_aliases - sttsbar.sh: Add padding to battery and ip to prevent text shifting position - .bash_aliases: Add x commands for keyboard layout + screensaver

--- scripts/sttsbar.sh
+++ scripts/sttsbar.sh
@@ -14,16 +14,18 @@ do
 	user="$(whoami)@$(hostname -s)"
 	workspace="[$(xprop -root _NET_CURRENT_DESKTOP | sed -E 's/[^0-9]+//')]"
 	if [ "$system" = "Linux" ]; then
-		ip=$(ip addr | grep 192.168.0.255 | cut -d " " -f 6 | cut -d "/" -f 1)
-		bigtemp=$(cat /sys/class/thermal/thermal_zone*/temp 2> /dev/null)
+		ipaddr=$(ip addr | grep 192.168.0.255 | cut -d " " -f 6 | cut -d "/" -f 1)
+		bigtemp=$(cat /sys/class/thermal/thermal_zone*/temp 2> /dev/null | head -n1)
 		cputemp="CPU: $((bigtemp/1000))°C"
 		batinfo=$(upower -e | grep BAT | head -n1)
-		battery="Bat: $(upower -i $batinfo | grep percentage | sed -E 's/[^0-9]+//')"
+		batper=$(upower -i $batinfo | grep percentage | sed -E 's/[^0-9]+//' | sed -E 's/%//')
 	elif [ "$system" = "OpenBSD" ]; then
-		ip=$(ifconfig | grep 192.168.0.255 | cut -d " " -f 2)
+		ipaddr=$(ifconfig | grep 192.168.0.255 | cut -d " " -f 2)
 		cputemp="CPU: $(sysctl hw.sensors.cpu0.temp0 | cut -d "=" -f 2 | cut -d "." -f 1)°C"
-		battery="Bat: $(apm -l)%"
+		batper=$(apm -l)
 	fi
+	battery="Bat: $(echo "$batper  " | cut -c1-3)%" # Pad to prevent text shifting
+	ip=$(echo "$ipaddr............." | cut -c1-13) # Pad, assumes max length of: 192.168.0.255
 	display="$user $workspace $whitespace $ip $separator $battery $separator $cputemp $separator $date $separator $clock $separator sttsbar.sh"
 	tput rc
 	echo "$display"
